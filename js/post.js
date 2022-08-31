c040066697feff4d44f20f706a3c1ea738f78753
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-09-01 00:29:45 +0600

    fetch

--- js/post.js
+++ js/post.js
@@ -4,15 +4,23 @@ function loadPosts() {
     .then((data) => displayposts(data));
 }
 function displayposts(posts) {
+  
+  //get the container
   const postContainer = document.getElementById("post-container");
   for (const post of posts) {
-    const postDiv=document.createElement('div');
-    postDiv.classList.add('post')
+    
+    //create the child element
+    const postDiv = document.createElement("div");
+    postDiv.classList.add("post");
+
+    //set inner html
     postDiv.innerHTML = `
     <h5>User-${post.userId}</h5>
     <h5>Post:${post.title}</h5>
     <p>Post Description:${post.body}</p>
     `;
+
+    //appendchild
     postContainer.appendChild(postDiv);
   }
 }
